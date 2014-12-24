58453e7e1db28e5046244f049c3465475cbf9d3c
alexknight <alexkn14@gmail.com>
2014-12-24 20:42:04 +0800

    add sixthday

---
+++ sixthday/parameterTesting.py
@@ -0,0 +1,14 @@
+'''
+Using the Python language, have the function LetterCapitalize(str) take the str parameter being
+passed and capitalize the first letter of each word. Words will be separated by only one space. 
+Use the Parameter Testing feature in the box below to test your code with different arguments.
+'''
+
+def LetterCapitalize(str):
+	str=str.split()
+	new_list=[]
+	for elem in str:
+		elem=elem[0].upper()+elem[1:]
+		new_list.append(elem)
+	return ' '.join(new_list)
+print LetterCapitalize(raw_input('input the sentence.\n'))
