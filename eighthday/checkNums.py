1cc5451c6442f88a0e30c91eaefdf6dbc4ed157a
alexknight <--add>
2014-12-25 10:41:48 +0800

    add eighthday

---
+++ eighthday/checkNums.py
@@ -0,0 +1,12 @@
+def CheckNums(num1,num2): 
+	if num1>num2:
+		return "true"
+	elif num1<num2:
+		return "false"
+	else:
+		return -1
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print CheckNums(raw_input())  
